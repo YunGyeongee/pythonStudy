7030d036b71427d8758f656386cdaa2a11295032
YunGyeong Kim <86589216+YunGyeongee@users.noreply.github.com>
2021-08-12 04:07:33 +0000

    introduction #0.1

---
+++ main.py
@@ -0,0 +1,12 @@
+a_string = 'like this' # we called String or text
+a_number = 3 # number
+a_float = 3.12 # float thing ~
+a_boolean = True # not text
+a_none = None # undifiend, nothing is well
+
+print(type(a_string))
+
+
+
+super_long_variable = True # snake case
+superLongVariable = True # JS
