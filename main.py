59542a838fc88ba09f9c2dc1589944aa458a4386
YunGyeong Kim <86589216+YunGyeongee@users.noreply.github.com>
2021-08-12 04:18:24 +0000

    Lists in Python

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ a_float = 3.12 # float thing ~
 a_boolean = True # not text
 a_none = None # undifiend, nothing is well
 
-print(type(a_string))
-
-
+#print(type(a_string))
 
 super_long_variable = True # snake case
 superLongVariable = True # JS
+
+
+
+# sequence type / sequence is like list
+days =["Mon", "Tue", "Wed", "Thur", "Fri"]
+print("Mon" in days)
+print(days[2]) # computer is count start from 0
+
+days.append("Sat")
+days.reverse()
+print(days)
